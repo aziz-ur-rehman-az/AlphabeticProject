515282b3bb009cc56afdf20e393ec197c9791e14
= <=>
2021-11-23 03:52:46 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,183 @@
+function a() {
+  document.getElementById("a");
+  document.getElementById("letter").innerHTML = "Aa";
+  document.getElementById("word").innerHTML = "Apple";
+  document.getElementById("picture").innerHTML =
+    "<img src='apple.png' width = 200px; height = 200px/>";
+}
+function b() {
+  document.getElementById("b");
+  document.getElementById("letter").innerHTML = "Bb";
+  document.getElementById("word").innerHTML = "Bat";
+  document.getElementById("picture").innerHTML =
+    "<img src='bat.png' width = 200px; height = 200px/>";
+}
+function c() {
+  document.getElementById("c");
+  document.getElementById("letter").innerHTML = "Cc";
+  document.getElementById("word").innerHTML = "Cat";
+  document.getElementById("picture").innerHTML =
+    "<img src='cat.png' width = 200px; height = 200px/>";
+}
+function d() {
+  document.getElementById("d");
+  document.getElementById("letter").innerHTML = "Dd";
+  document.getElementById("word").innerHTML = "Dog";
+  document.getElementById("picture").innerHTML =
+    "<img src='dog.png' width = 200px; height = 200px/>";
+}
+function e() {
+  document.getElementById("e");
+  document.getElementById("letter").innerHTML = "Ee";
+  document.getElementById("word").innerHTML = "Elephant";
+  document.getElementById("picture").innerHTML =
+    "<img src='elephant.png' width = 200px; height = 200px/>";
+}
+function f() {
+  document.getElementById("f");
+  document.getElementById("letter").innerHTML = "Ff";
+  document.getElementById("word").innerHTML = "Fish";
+  document.getElementById("picture").innerHTML =
+    "<img src='fish.png' width = 200px; height = 200px/>";
+}
+function g() {
+  document.getElementById("g");
+  document.getElementById("letter").innerHTML = "Gg";
+  document.getElementById("word").innerHTML = "Glass";
+  document.getElementById("picture").innerHTML =
+    "<img src='glass.png' width = 200px; height = 200px/>";
+}
+function h() {
+  document.getElementById("h");
+  document.getElementById("letter").innerHTML = "Hh";
+  document.getElementById("word").innerHTML = "Hen";
+  document.getElementById("picture").innerHTML =
+    "<img src='hen.png' width = 200px; height = 200px/>";
+}
+
+function i() {
+  document.getElementById("i");
+  document.getElementById("letter").innerHTML = "Ii";
+  document.getElementById("word").innerHTML = "Ink";
+  document.getElementById("picture").innerHTML =
+    "<img src='ink.png' width = 200px; height = 200px/>";
+}
+function j() {
+  document.getElementById("j");
+  document.getElementById("letter").innerHTML = "Jj";
+  document.getElementById("word").innerHTML = "Jug";
+  document.getElementById("picture").innerHTML =
+    "<img src='jug.png' width = 200px; height = 200px/>";
+}
+function k() {
+  document.getElementById("k");
+  document.getElementById("letter").innerHTML = "Kk";
+  document.getElementById("word").innerHTML = "Kite";
+  document.getElementById("picture").innerHTML =
+    "<img src='kite.png' width = 200px; height = 200px/>";
+}
+function l() {
+  document.getElementById("l");
+  document.getElementById("letter").innerHTML = "Ll";
+  document.getElementById("word").innerHTML = "Lion";
+  document.getElementById("picture").innerHTML =
+    "<img src='lion.png' width = 200px; height = 200px/>";
+}
+function m() {
+  document.getElementById("m");
+  document.getElementById("letter").innerHTML = "Mm";
+  document.getElementById("word").innerHTML = "Monkey";
+  document.getElementById("picture").innerHTML =
+    "<img src='monkey.png' width = 200px; height = 200px/>";
+}
+function n() {
+  document.getElementById("n");
+  document.getElementById("letter").innerHTML = "Nn";
+  document.getElementById("word").innerHTML = "Nose";
+  document.getElementById("picture").innerHTML =
+    "<img src='nose.png' width = 200px; height = 200px/>";
+}
+function o() {
+  document.getElementById("o");
+  document.getElementById("letter").innerHTML = "Oo";
+  document.getElementById("word").innerHTML = "Owl";
+  document.getElementById("picture").innerHTML =
+    "<img src='owl.png' width = 200px; height = 200px/>";
+}
+function p() {
+  document.getElementById("p");
+  document.getElementById("letter").innerHTML = "Pp";
+  document.getElementById("word").innerHTML = "Pencil";
+  document.getElementById("picture").innerHTML =
+    "<img src='pencil.png' width = 200px; height = 200px/>";
+}
+function q() {
+  document.getElementById("q");
+  document.getElementById("letter").innerHTML = "Qq";
+  document.getElementById("word").innerHTML = "Queen";
+  document.getElementById("picture").innerHTML =
+    "<img src='queen.png' width = 200px; height = 200px/>";
+}
+function r() {
+  document.getElementById("r");
+  document.getElementById("letter").innerHTML = "Rr";
+  document.getElementById("word").innerHTML = "Rabbit";
+  document.getElementById("picture").innerHTML =
+    "<img src='rabbit.png' width = 200px; height = 200px/>";
+}
+function s() {
+  document.getElementById("s");
+  document.getElementById("letter").innerHTML = "Ss";
+  document.getElementById("word").innerHTML = "Sun";
+  document.getElementById("picture").innerHTML =
+    "<img src='sun.png' width = 200px; height = 200px/>";
+}
+function t() {
+  document.getElementById("t");
+  document.getElementById("letter").innerHTML = "Tt";
+  document.getElementById("word").innerHTML = "Tiger";
+  document.getElementById("picture").innerHTML =
+    "<img src='tiger.png' width = 200px; height = 200px/>";
+}
+function u() {
+  document.getElementById("u");
+  document.getElementById("letter").innerHTML = "Uu";
+  document.getElementById("word").innerHTML = "Umbrella";
+  document.getElementById("picture").innerHTML =
+    "<img src='umbrella.png' width = 200px; height = 200px/>";
+}
+function v() {
+  document.getElementById("v");
+  document.getElementById("letter").innerHTML = "Vv";
+  document.getElementById("word").innerHTML = "Van";
+  document.getElementById("picture").innerHTML =
+    "<img src='van.png' width = 200px; height = 200px/>";
+}
+function w() {
+  document.getElementById("w");
+  document.getElementById("letter").innerHTML = "Ww";
+  document.getElementById("word").innerHTML = "Window";
+  document.getElementById("picture").innerHTML =
+    "<img src='window.png' width = 200px; height = 200px/>";
+}
+function x() {
+  document.getElementById("x");
+  document.getElementById("letter").innerHTML = "Xx";
+  document.getElementById("word").innerHTML = "X-Ray";
+  document.getElementById("picture").innerHTML =
+    "<img src='xray.png' width = 200px; height = 200px/>";
+}
+function y() {
+  document.getElementById("y");
+  document.getElementById("letter").innerHTML = "Yy";
+  document.getElementById("word").innerHTML = "Yak";
+  document.getElementById("picture").innerHTML =
+    "<img src='yak.png' width = 200px; height = 200px/>";
+}
+function z() {
+  document.getElementById("z");
+  document.getElementById("letter").innerHTML = "Zz";
+  document.getElementById("word").innerHTML = "Zebra";
+  document.getElementById("picture").innerHTML =
+    "<img src='zebra.png' width = 200px; height = 200px/>";
+}
